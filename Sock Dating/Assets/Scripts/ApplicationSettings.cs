ee4cd4c36e68fa85efa2001256d123d5f4614b5d
Jon <j7caiman@gmail.com>
2021-02-08 03:04:23 -0500

    navigation & town art complete

---
+++ Sock Dating/Assets/Scripts/ApplicationSettings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+
+public class ApplicationSettings : MonoBehaviour {
+    void Start() {
+        Application.targetFrameRate = 30;
+    }
+}
